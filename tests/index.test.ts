a8bb5e233bdbb0f9ef3f671cff5e260cde03c447
lantos1618 <l.leong1618@gmail.com>
2023-05-21 12:22:26 +0100

    update ts files

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,5 @@
 import { Authorization } from '../src/api/authorization';
-import { Checkout } from '../src/checkout';
+import { Checkout } from '../src/api/checkout';
 import { SumUp } from '../src/sumup';
 import dotenv from 'dotenv';
 
@@ -11,7 +11,14 @@ describe('SumUp', () => {
         if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.API_BASE_URL) {
             throw new Error('Missing environment variables');
         }
-        sumUp = new SumUp(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.API_BASE_URL);
+        sumUp = new SumUp(new Authorization({
+            kind: 'bearer',
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            apiBaseURL: process.env.API_BASE_URL,
+            apiVersion: '/v0.1',
+        }));
+        
     });
 
 
